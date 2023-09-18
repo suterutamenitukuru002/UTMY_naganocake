d1604c1b0f3f82fd126eb8c0fd3385d9f9bda1a0
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-09-18 17:03:45 +0000

    [add]新規登録ページ/トップページ/アバウトページルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   sessions: 'public/sessions'
   }
 
+  # 会員側のルーティング設定
+  scope module: :public do
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about'
+  end
+
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
