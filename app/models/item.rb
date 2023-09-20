95201f2e29f2263074ad65db55965225ef5e1d5d
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-09-20 16:00:11 +0000

    [update]model/item.rbの更新と商品実装の更新

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
 
+   scope :price_high_to_low, -> { order(price: :desc) }
+   scope :price_low_to_high, -> { order(price: :asc) }
 end
