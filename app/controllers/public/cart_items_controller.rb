86654fd4250d29fef91d4a28d98265c8b3c4d756
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-09-21 05:07:02 +0000

    [add]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,8 @@
 class Public::CartItemsController < ApplicationController
+    before_action :authenticate_customer!
+    before_action :set_cart_item, only: %i[increase decrease destroy]
+
   def index
+    @cart_items = current_customer.cart_items
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
 
+
    scope :price_high_to_low, -> { order(price: :desc) }
    scope :price_low_to_high, -> { order(price: :asc) }
+   has_many :cart_items, dependent: :destroy
 end
