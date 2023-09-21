f7f00b0b88f09d3d6bf22fb8ef0d9d5faee8263c
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-09-21 05:00:09 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,54 @@
 Rails.application.routes.draw do
 
+  # 配送先情報
+  # namespace :public do
+  #   # get 'addresses/index'
+  #   # get 'addresses/:id/edit',to: 'addresses#edit'
+  #   # patch 'addresses/:id', to: 'addresses#update'
+  #   # post 'addresses/create'
+  #   # delete '/addresses/:id', to: 'addresses#destroy'
+  # end
+
+  # 注文情報
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/check'
+    get 'orders/complete'
+  end
+
+  # カート内商品
+  namespace :public do
+    get 'cart_items/index'
+  end
+
+  # 顧客情報ページ
+  namespace :public do
+    get 'customers/mypage'
+    get 'customers/edit'
+    get 'customers/check'
+    patch 'customers/is_withdraw'
+    patch 'customers/infomation'
+  end
+
+
+
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+
+  # 会員側のルーティング設定
+  scope module: :public do
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about'
+
+
+    # 商品ページを作成する際に必要
+    resources :items, only: %i[index show]
+    resources :addresses,only: [:index,:create,:edit,:update,:destroy]
+  end
 
 
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -25,6 +70,8 @@ Rails.application.routes.draw do
     get 'orders/show'
   end
   namespace :admin do
-    resources :customers, only: [:index, :edit, :show, :update]
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
   end
 end
