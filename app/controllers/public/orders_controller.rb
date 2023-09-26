3d07f10cca0f4303e898f14622f8490a13c212d4
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-09-26 13:46:46 +0000

    コントローラー修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,27 +4,35 @@ class Public::OrdersController < ApplicationController
     @addresses = current_customer.addresses.all
   end
 
-  def create
+def create
   cart_items = current_customer.cart_items.all
   @order = current_customer.orders.new(order_params)
   @order.order_status = 0
-    if @order.save
-    current_customer.cart_items.each do |cart_item| #カート内商品を1つずつ取り出しループ
-    @ordered_item = OrderDetail.new #初期化宣言
-    @ordered_item.order_id = @order.id #order注文idを紐付けておく
-    @ordered_item.item_id = cart_item.item_id #カート内商品idを注文商品idに代入
-    @ordered_item.quantity = cart_item.amount #カート内商品の個数を注文商品の個数に代入
-    @ordered_item.price = (cart_item.item.price*1.1).floor #消費税込みに計算して代入
-    @ordered_item.save
+  if @order.save
+      current_customer.cart_items.each do |cart_item| #カート内商品を1つずつ取り出しループ
+        @ordered_item = OrderDetail.new #初期化宣言
+        @ordered_item.order_id =  @order.id #order注文idを紐付けておく
+        @ordered_item.item_id = cart_item.item_id #カート内商品idを注文商品idに代入
+        @ordered_item.quantity = cart_item.amount #カート内商品の個数を注文商品の個数に代入
+        @ordered_item.price = (cart_item.item.price*1.1).floor #消費税込みに計算して代入
+        @ordered_item.save
+      end
+        redirect_to public_orders_complete_path
+        cart_items.destroy_all
+    # ユーザーに関連するカートのデータ(購入したデータ)をすべて削除します(カートを空にする)
+      else
+      render 'new'
+    end
+    @cart_items = current_customer.cart_items.all
+    @shipping_fee = 800
+     ary = []
+   @cart_items.each do |cart_item|
+    ary << (cart_item.item.price*1.1 *cart_item.amount).floor
    end
-    redirect_to public_orders_complete_path
-    cart_items.destroy_all
-  # ユーザーに関連するカートのデータ(購入したデータ)をすべて削除します(カートを空にする)
-   else
-    @order = Order.new(order_params)
-    render :new
-  end
-  end
+   @cart_items_price = ary.sum
+   @selected_pay_method = params[:order][:payment_method]
+   @total_price = @shipping_fee + @cart_items_price
+ end
 
 
  def index
@@ -69,39 +77,6 @@ class Public::OrdersController < ApplicationController
       @selected_pay_method = params[:order][:payment_method]
       @total_price = @shipping_fee + @cart_items_price
   end
-
-def create
-  cart_items = current_customer.cart_items.all
-  @order = current_customer.orders.new(order_params)
-  @order.order_status = 0
-  if @order.save
-      current_customer.cart_items.each do |cart_item| #カート内商品を1つずつ取り出しループ
-        @ordered_item = OrderDetail.new #初期化宣言
-        @ordered_item.order_id =  @order.id #order注文idを紐付けておく
-        @ordered_item.item_id = cart_item.item_id #カート内商品idを注文商品idに代入
-        @ordered_item.quantity = cart_item.amount #カート内商品の個数を注文商品の個数に代入
-        @ordered_item.price = (cart_item.item.price*1.1).floor #消費税込みに計算して代入
-        @ordered_item.save
-      end
-        redirect_to public_orders_complete_path
-        cart_items.destroy_all
-    # ユーザーに関連するカートのデータ(購入したデータ)をすべて削除します(カートを空にする)
-
-      else
-      render 'new'
-    end
-    @cart_items = current_customer.cart_items.all
-    @shipping_fee = 800
-     ary = []
-   @cart_items.each do |cart_item|
-    ary << (cart_item.item.price*1.1 *cart_item.amount).floor
-   end
-   @cart_items_price = ary.sum
-   @selected_pay_method = params[:order][:payment_method]
-   @total_price = @shipping_fee + @cart_items_price
- end
-
-
 
  def complete
  end
