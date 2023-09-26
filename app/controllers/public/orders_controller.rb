a53a2cadf24f7f2a72feda0ad39926a63e3f2b00
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-09-26 13:22:44 +0000

    レイアウト実装

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,15 +1,15 @@
 class Public::OrdersController < ApplicationController
- def new
-  @order = Order.new
-  @addresses = current_customer.addresses.all
- end
+  def new
+    @order = Order.new
+    @addresses = current_customer.addresses.all
+  end
 
-def create
- cart_items = current_customer.cart_items.all
- @order = current_customer.orders.new(order_params)
- @order.order_status = 0
- if @order.save
-   current_customer.cart_items.each do |cart_item| #カート内商品を1つずつ取り出しループ
+  def create
+  cart_items = current_customer.cart_items.all
+  @order = current_customer.orders.new(order_params)
+  @order.order_status = 0
+    if @order.save
+    current_customer.cart_items.each do |cart_item| #カート内商品を1つずつ取り出しループ
     @ordered_item = OrderDetail.new #初期化宣言
     @ordered_item.order_id = @order.id #order注文idを紐付けておく
     @ordered_item.item_id = cart_item.item_id #カート内商品idを注文商品idに代入
@@ -38,7 +38,7 @@ def create
   @address = @order.postcode + @order.address + @order.address_name
  end
 
- def check
+  def check
     @order = Order.new(order_params)
   # [:address_option]=="0"のデータ(memberの住所)を呼び出す
     if params[:order][:address_option] == "0"
@@ -52,10 +52,23 @@ def create
       @order.address = @address.address
       @order.address_name = @address.name
 
-      elsif params[:order][:address_option] = "2"
-       @order.postcode = params[:order][:postcode]
-       @order.address = params[:order][:address]
-       @order.address_name = params[:order][:name]
+    elsif params[:order][:address_option] = "2"
+      @order.postcode = params[:order][:postcode]
+      @order.address = params[:order][:address]
+      @order.address_name = params[:order][:name]
+    else
+      render 'new'
+    end
+      @cart_items = current_customer.cart_items.all
+      @shipping_fee = 800
+        ary = []
+      @cart_items.each do |cart_item|
+      ary << (cart_item.item.price*1.1 *cart_item.amount).floor
+      end
+      @cart_items_price = ary.sum
+      @selected_pay_method = params[:order][:payment_method]
+      @total_price = @shipping_fee + @cart_items_price
+  end
 
 def create
   cart_items = current_customer.cart_items.all
@@ -96,11 +109,11 @@ def create
 private
 
 def order_params
- params.require(:order).permit(:address_name, :address, :postcode, :payment_method, :billing_amount, :postage)
+  params.require(:order).permit(:address_name, :address, :postcode, :payment_method, :billing_amount, :postage)
 end
 
 def address_params
- params.require(:order).permit(:name, :address, :postcode)
+  params.require(:order).permit(:name, :address, :postcode)
 end
 
 end
